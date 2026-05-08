973bd59b43cfdb6b46704f30dc2999da1fba8a5c
asnayem1122 <asnayem1122@gmail.com>
2026-05-08 20:45:02 +0600

    Updated Code

--- LCS_String_Length.cpp
+++ LCS_String_Length.cpp
@@ -1,68 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> dp;
+vector<vector<int>>dp;
+string s3;
 
-int LCS(int i, int j, string &s1, string &s2){
-
-    // base case
+int LCS(int i , int j, string s1, string s2){
     if(i == s1.length() || j == s2.length()){
         return 0;
     }
-
-    // memoization
-    if(dp[i][j] != -1){
-        return dp[i][j];
-    }
-
-    // match
+    int take = 0, skip = 0;
+    
     if(s1[i] == s2[j]){
-        return dp[i][j] = 1 + LCS(i+1, j+1, s1, s2);
+        take =  1 + LCS(i+1, j+1, s1, s2);
+        s3+=s1[i];
+        return take;
     }
 
-    // not match
-    return dp[i][j] = max(
-        LCS(i+1, j, s1, s2),
-        LCS(i, j+1, s1, s2)
-    );
+    skip =  max(LCS(i+1, j, s1, s2), LCS(i, j+1, s1, s2));
+    return skip;
 }
 
+
 int main() {
-
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
     string s1, s2;
-    cin >> s1 >> s2;
+    cin>>s1>>s2;
 
-    dp.assign(s1.length()+1,
-              vector<int>(s2.length()+1, -1));
+    dp.assign(s1.length()+1, vector<int>(s2.length(), -1));
 
-    cout << "Length = " << LCS(0, 0, s1, s2) << endl;
+    cout<<LCS(0, 0, s1, s2)<<endl;
 
-    // reconstruct LCS string
-    int i = 0, j = 0;
-    string ans = "";
+    reverse(s3.begin(), s3.end());
 
-    while(i < s1.length() && j < s2.length()){
-
-        // character matched
-        if(s1[i] == s2[j]){
-            ans += s1[i];
-            i++;
-            j++;
-        }
-
-        // move according to larger dp value
-        else if(dp[i+1][j] > dp[i][j+1]){
-            i++;
-        }
-        else{
-            j++;
-        }
-    }
-
-    cout << "LCS = " << ans << endl;
+    cout<<s3;
 
     return 0;
 }
